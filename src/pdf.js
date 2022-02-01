7c0444a0b85d84dc31714b895d983dc6aa08699c
Erick <erickfco1999m@gmail.com>
2022-02-01 00:13:28 -0600

    visor pdf

---
+++ src/pdf.js
@@ -0,0 +1,11 @@
+document.addEventListener("adobe_dc_view_sdk.ready", function(){
+    var adobeDCView = new AdobeDC.View({clientId: "2a2cd192381d4724add32fb82342bb05", divId: "adobe-dc-view"});
+    adobeDCView.previewFile({
+    content:{ location:
+        { url: "pdf/Ejemplo-Reporte-semanal.pdf"}},
+    metaData:{fileName: "Ejemplo-Reporte-semanal.pdf"}
+    },
+    {
+    embedMode: "SIZED_CONTAINER"
+    });
+});
